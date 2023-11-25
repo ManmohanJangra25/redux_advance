19c36a61b34fad90da80ed419a926f878d053506
Manmohan <manmohanjangra2528@gmail.com>
2023-11-25 19:09:35 +0530

    Redux Advance Project setup and 2 modules done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,20 @@
+import { useSelector } from 'react-redux';
 import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
 
 const Cart = (props) => {
+  const cartItems = useSelector(state => state.cart.items);
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
+      {cartItems.map((data) => (
         <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
+          key={data.id}
+          item={{ id: data.id, title: data.title, quantity: data.quantity, total: data.totalPrice, price: data.price }}
         />
+      ))}
       </ul>
     </Card>
   );
